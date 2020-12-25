3a6999fbec85300e612a7440d8af16b288f8905b
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2020-12-25 12:47:17 -0500

    Update src/add_per_file.py

--- src/add_per_file.py
+++ src/add_per_file.py
@@ -2,8 +2,6 @@
 
 from datetime import datetime
 import subprocess
-import os
-from os.path import basename
 
 # Return current_time
 def current_time() -> str:
@@ -14,13 +12,12 @@ def current_time() -> str:
 # Set a simple commit message per file pertaining to that file's name
 # Push
 def add_per_file() -> None:
-    project_name = basename(os.getcwd())
     subprocess.call(["git"] + ["add", "."])
 
     file_list = subprocess.check_output(["git"] + ["diff"] + ["--cached"] + ["--name-only"], text=True).split("\n")[:-1]
-    for file in file_list:
-        commit_message = "Update " + file + " in " + project_name
-        filepath = "./" + file
+    for git_file in file_list:
+        commit_message = "Update " + git_file
+        filepath = "./" + git_file
         subprocess.call(["git"] + ["commit", "-m", commit_message] + [filepath])
 
     subprocess.call(["git"] + ["push"])
